4168982ecec5b2ccb26bee9b18322e831287d927
Folami <folami.bidmus@gmail.com>
2025-04-01 20:12:38 +0100

    [Respond with 200]

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 import socket  # noqa: F401
 
-
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    
+    while True:
+        conn, addr = server_socket.accept()
+        print("Accepted connection from", addr)
+        response = "HTTP/1.1 200 OK\r\n\r\n"
+        conn.sendall(response.encode("utf-8"))
+        conn.close()
 
 if __name__ == "__main__":
     main()
